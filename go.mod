99275e23607f8548fc7f2086c0de6a255f3cea71
Minh <nbminh2101@gmail.com>
2023-08-20 11:56:39 +0700

    test-export-pdf

---
+++ go.mod
@@ -0,0 +1,3 @@
+module test-export-pdf
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func ExportWordToPDF(wordFilePath, pdfOutputDir string) (string, error) {
+	libreofficePath := "C:\\Program Files\\LibreOffice\\program\\swriter.exe"
+	if _, err := os.Stat(libreofficePath); os.IsNotExist(err) {
+		return "", fmt.Errorf("LibreOffice executable not found")
+	}
+
+	pdfFilePath := filepath.Join(pdfOutputDir, "test.pdf")
+
+	cmd := exec.Command(libreofficePath, "--convert-to", "pdf", "--outdir", pdfOutputDir, wordFilePath)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("Error exporting Word to PDF: %s\nOutput: %s", err, string(output))
+	}
+
+	return pdfFilePath, nil
+}
+
+func HandleConvertAndDownload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	wordFilePath := "./test.docx"
+	pdfOutputDir := "./"
+
+	pdfFilePath, err := ExportWordToPDF(wordFilePath, pdfOutputDir)
+	if err != nil {
+		http.Error(w, "Error exporting Word to PDF", http.StatusInternalServerError)
+		return
+	}
+
+	pdfFile, err := os.Open(pdfFilePath)
+	if err != nil {
+		http.Error(w, "Error opening PDF file", http.StatusInternalServerError)
+		return
+	}
+	defer func() {
+		if err = os.Remove(pdfFilePath); err != nil {
+			log.Printf("Error removing temporary PDF file: %s", err)
+		}
+	}()
+	defer pdfFile.Close()
+
+	w.Header().Set("Content-Type", "application/pdf")
+	w.Header().Set("Content-Disposition", "attachment; filename=output.pdf")
+
+	_, err = io.Copy(w, pdfFile)
+	if err != nil {
+		http.Error(w, "Error sending PDF content", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("Converted and sent PDF successfully")
+}
+
+func main() {
+	http.HandleFunc("/export-pdf", HandleConvertAndDownload)
+
+	fmt.Println("Server is listening on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
